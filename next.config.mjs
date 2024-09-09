49d348193db165bcb42b2225e6dd8d271820ecf3
Robert Melendez <robertm3lendez@gmail.com>
2024-09-09 17:38:36 -0400

    assets json

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        headers() {
-          return [
+   experimental: {
+      headers() {
+         return [
             {
-              source: "/.well-known/apple-app-site-association",
-              headers: [{ key: "content-type", value: "application/json" }]
-            }
-          ];
-        }
-      }
+               source: '/.well-known/apple-app-site-association',
+               headers: [{ key: 'content-type', value: 'application/json' }],
+            },
+            {
+               source: '/.well-known/assetlinks.json',
+               headers: [{ key: 'content-type', value: 'application/json' }],
+            },
+         ];
+      },
+   },
 };
 
 export default nextConfig;

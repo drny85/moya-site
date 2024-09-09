c40307906444edff676634a451e51dfd06a48c09
Robert Melendez <robertm3lendez@gmail.com>
2024-09-09 16:36:01 -0400

    padding

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen min-w-full flex-col items-center justify-between bg-white">
       <img src="/images/banner.png"  alt="banner" className="w-full h-full" />
-      <section className="w-full max-w-md mt-8 sm:hidden  text-center absolute z-10 bottom-6">
+      <section className="w-full max-w-md mt-8 sm:hidden  text-center absolute z-10 bottom-6 p-4">
         <h2 className="text-xl font-semibold text-gray-800">Download the App Now!</h2>
         <p className="text-gray-600 mt-4 text-sm">Download Moya Barber app now. Available on both iOS and Android.</p>
         <div className="mt-8 items-center justify-center space-x-8 flex h-40">
